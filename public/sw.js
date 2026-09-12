3205463d05f81a9ec87acd8681163e5e31b39bfb
lorongsatu21-tech <lorongsatu21@gmail.com>
2026-09-13 06:41:31 +0800

    Delete public directory

--- public/sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE = 'ls21-v1';
-const ASSETS = ['./','./index.html','./css/app.css','./js/app.js','./js/store.js','./manifest.json','./icons/icon.svg'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
