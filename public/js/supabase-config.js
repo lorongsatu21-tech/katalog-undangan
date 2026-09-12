b63690c8ff605ce5a64aa53c6cc49d2bb028d8a2
lorongsatu21-tech <lorongsatu21@gmail.com>
2026-09-13 06:02:19 +0800

    Update supabase-config.js

--- public/js/supabase-config.js
+++ public/js/supabase-config.js
@@ -1,1 +1,6 @@
-
+window.LS21_SUPABASE = {
+  url: 'https://nrdzkojcheutyyeuzugv.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yZHprb2pjaGV1dHl5ZXV6dWd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyMzE1MTAsImV4cCI6MjEwNDgwNzUxMH0.BP8eCGNg8yIBI3zpgiY7ECBilQJRwfU_GHxQ9YjzXxA',
+  adminUser: 'admin',
+  adminPass: 'lorongsatu21'
+};
